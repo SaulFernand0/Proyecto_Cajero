a8fdd7c85aadc9329e5d3114f7869d3aad790a42
SaulFernand0 <saulolivas@upeu.edu.pe>
2020-05-23 15:07:40 -0500

    cajero base

---
+++ js/teclas.js
@@ -0,0 +1,96 @@
+document.getElementById("n1").addEventListener("click",n1);
+document.getElementById("n2").addEventListener("click",n2);
+document.getElementById("n3").addEventListener("click",n3);
+document.getElementById("n4").addEventListener("click",n4);
+document.getElementById("n5").addEventListener("click",n5);
+document.getElementById("n6").addEventListener("click",n6);
+document.getElementById("n7").addEventListener("click",n7);
+document.getElementById("n8").addEventListener("click",n8);
+document.getElementById("n9").addEventListener("click",n9);
+document.getElementById("n10").addEventListener("click",n10);
+document.getElementById("n11").addEventListener("click",n11);
+document.getElementById("n12").addEventListener("click",n12);
+
+function n1(){ 
+    let sumado = document.getElementById("n1").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n2(){ 
+    let sumado = document.getElementById("n2").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n3(){ 
+    let sumado = document.getElementById("n3").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n4(){ 
+    let sumado = document.getElementById("n4").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n5(){ 
+    let sumado = document.getElementById("n5").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n6(){ 
+    let sumado = document.getElementById("n6").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n7(){ 
+    let sumado = document.getElementById("n7").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n8(){ 
+    let sumado = document.getElementById("n8").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n9(){ 
+    let sumado = document.getElementById("n9").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n10(){ 
+    let sumado = document.getElementById("n10").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n11(){ 
+    let sumado = document.getElementById("n11").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
+
+function n12(){ 
+    let sumado = document.getElementById("n12").innerHTML;
+    var elemento = document.getElementById("password");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+}
