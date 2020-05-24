e99cbbdd8b0db8b663e93730233019bd95d2aa01
SaulFernand0 <saulolivas@upeu.edu.pe>
2020-05-24 03:53:39 -0500

    actualizacion 5

---
+++ js/funcionespagina.js
@@ -0,0 +1,27 @@
+function gracias(){
+    window.location = "gracias.html";
+}
+
+function ingresarmonto(){
+    window.location = "ingresarmonto.html";
+}
+
+function MenuPrincipal(){
+    window.location = "MenuPrincipal.html";
+}
+
+function otrascantidades(){
+    window.location = "otrascantidades.html";
+}
+
+function retiro(){
+    window.location = "retiro.html";
+}
+
+function retirosoles(){
+    window.location = "retirosoles.html";
+}
+
+function transaccionprocesada(){
+    window.location = "transaccionprocesada.html";
+}
