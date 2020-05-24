8d604d8dfa67252bb038c37d73bc1fce3f5e9962
SaulFernand0 <saulolivas@upeu.edu.pe>
2020-05-24 18:39:28 -0500

    actualizacion 7

---
+++ js/ok.js
@@ -0,0 +1,17 @@
+contraseña = "123456";
+
+function ok(){
+    document.getElementById("password").value;
+    if(document.getElementById("password").value  == contraseña){
+       alert("contraseña correcta");
+       MenuPrincipal();
+    }else{
+        document.getElementById("password");
+        document.value = "";
+        alert("contraseña incorrecta")
+    }
+}
+
+function MenuPrincipal(){
+    window.location = "MenuPrincipal.html";
+}
